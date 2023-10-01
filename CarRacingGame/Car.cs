9baa78821a3700dca90a22053602632ab47ccd2c
Dmitriy7733 <Dmitrii@yandex.ru>
2023-10-02 00:26:04 +0300

    Add project files.

---
+++ CarRacingGame/Car.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+namespace CarRacingGame
+{
+
+    // Определение делегата для события "Финиш"
+    public delegate void FinishEventHandler(object sender, EventArgs e);
+    
+    public class Car 
+    {
+        private static int finishCounter = 0;
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public int FinishOrder { get; private set; } // Порядковый номер финишировавшего автомобиля
+
+        public event FinishEventHandler Finish; // Событие, вызываемое при достижении финиша
+
+        public void Move()
+        {
+            Random random = new Random();
+            for (int distance = 0; distance <= 110; distance += Speed)
+            {
+                Console.WriteLine($"{Model} двигается. Пройдено {distance} км.");
+                Thread.Sleep(100); // Задержка для визуализации движения
+
+                if (distance >= 100)
+                {
+                                        FinishOrder = Interlocked.Increment(ref finishCounter); // Увеличиваем порядковый номер финишировавшего автомобиля
+                    OnFinish(); // Вызываем событие при достижении финиша
+                    break;
+                }
+
+                Speed = random.Next(5, 20); // Изменение скорости случайным образом
+            }
+        }
+
+        protected virtual void OnFinish()
+        {
+            Finish?.Invoke(this, EventArgs.Empty); // Вызываем событие "Финиш"
+        }
+    }
+
+}
---
+++ CarRacingGame/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using CarRacingGame;
+using System.Reflection;
+
+class Program
+{
+    static string[] finishLabels = { "первым", "вторым", "третьим", "четвертым" }; // Массив текстовых меток
+
+    static void Main()
+    {
+        Car sportsCar = new Car { Model = "Спортивный автомобиль" };
+        Car sedanCar = new Car { Model = "Легковой автомобиль" };
+        Car truck = new Car { Model = "Грузовик" };
+        Car bus = new Car { Model = "Автобус" };
+
+        // Запуск движение каждого автомобиля параллельно
+        Thread sportsCarThread = new Thread(sportsCar.Move);
+        Thread sedanCarThread = new Thread(sedanCar.Move);
+        Thread truckThread = new Thread(truck.Move);
+        Thread busThread = new Thread(bus.Move);
+
+        // Подписка на событие "Финиш" для каждого автомобиля после создания автомобилей
+        sportsCar.Finish += (sender, e) =>
+        {
+            int order = ((Car)sender).FinishOrder;
+            string label = finishLabels[order - 1];
+            Console.WriteLine($"{((Car)sender).Model} пришел {label}!");
+        };
+
+        sedanCar.Finish += (sender, e) =>
+        {
+            int order = ((Car)sender).FinishOrder;
+            string label = finishLabels[order - 1];
+            Console.WriteLine($"{((Car)sender).Model} пришел {label}!");
+        };
+
+        truck.Finish += (sender, e) =>
+        {
+            int order = ((Car)sender).FinishOrder;
+            string label = finishLabels[order - 1];
+            Console.WriteLine($"{((Car)sender).Model} пришел {label}!");
+        };
+
+        bus.Finish += (sender, e) =>
+        {
+            int order = ((Car)sender).FinishOrder;
+            string label = finishLabels[order - 1];
+            Console.WriteLine($"{((Car)sender).Model} пришел {label}!");
+        };
+
+        sportsCarThread.Start();
+        sedanCarThread.Start();
+        truckThread.Start();
+        busThread.Start();
+
+
+        sportsCarThread.Join();
+        sedanCarThread.Join();
+        truckThread.Join();
+        busThread.Join();
+    }
+}
